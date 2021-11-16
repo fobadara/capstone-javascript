eb195bd7a29246f6369a131b90235e7df897d91f
Abdulfatai <fob90s@gmail.com>
2021-11-16 15:48:05 +0100

    Update webpack config files

--- src/index.js
+++ src/index.js
@@ -2,10 +2,6 @@ import './style.css';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-if (process.env.NODE_ENV !== 'production') {
-  console.log('Looks like we are in development mode!');
-}
-
 function sum(a, b) {
   return a + b;
 }
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,10 +1,11 @@
+/* eslint-disable */
 const { merge } = require('webpack-merge');
- const common = require('./webpack.common.js');
+const common = require('./webpack.common.js');
 
- module.exports = merge(common, {
-   mode: 'development',
-   devtool: 'inline-source-map',
-   devServer: {
-     static: './dist',
-   },
- });
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+});
