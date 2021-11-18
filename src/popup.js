e780872a9dfbc51205391ea15f690e99e5fc280b
Gordon <gordon.tinyefuza@gmail.com>
2021-11-18 12:00:30 +0300

    Add getCountries function

--- src/popup.js
+++ src/popup.js
@@ -65,3 +65,47 @@ const loadLike = (id, node) => {
     }
   })
 }
+
+const getFood = async () => {
+  const response = await fetch ("");
+  response.json().then((json) => {
+    let itemArr = json.meals;
+    let itemHtml = '';
+    itemArr.forEach(item => {
+      itemHtml += 
+      `
+      <div class="col-4">
+        <div class="row">
+          <div class="col-12 item-img">
+            <img src=${item.strMealThumb} alt="item image">
+          </div>
+          <div class="col-12 text-center">
+            <p>${item.strMeal}</p>
+          </div>
+          <div class="col-6 item-like">
+            <img id="${item.idMeal}" src="https://img.icons8.com/material-outlined/24/000000/like--v2.png" />
+            <span>likes</span>
+          </div>
+          <div class="col-6 text-left">
+            <button type="button">Comment</button>
+          </div>
+        </div>
+      </div>`;
+    });
+
+    const itemContainer = document.querySelector('#items');
+    itemContainer.innerHTML = itemHtml;
+
+    const itemLike = document.querySelectorAll(".item-like > img");
+    itemLike.forEach((item) => {
+      let likeNode = item.parentNode.querySelector("span");
+      loadLike(item.id, likeNode);
+      item.addEventListener("click", () => {
+        let likeNode = item.parentNode.querySelector("span");
+        getLike(item.id, likeNode);
+      })
+    })
+  })
+}
+
+getFood();
