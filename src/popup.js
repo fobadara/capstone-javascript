76e3c8fb0abd16babbdb2e2e1ec77d674f2d59c5
Gordon <gordon.tinyefuza@gmail.com>
2021-11-18 11:56:22 +0300

    Add getLike function

--- src/popup.js
+++ src/popup.js
@@ -17,3 +17,28 @@ response.json().then((json) => {
   console.log(json);
 });
 }
+
+const getLike = (id, none) => {
+  console.log(node);
+  fetch("")
+  .then(response => response.json())
+  .then(json => {
+    let bool = false;
+    let idx = 0;
+    let likenum = -1;
+    json.forEach((item, i) => {
+      if (item.item_id === id){
+        bool = true;
+        idx = i;
+        likenum = item.likes;
+      }
+    })
+    if(bool){
+      node.textContent = `like ${likenum + 1}`;
+      addLike(id);
+    }else if (bool == false) {
+      node.textContent = `like 1`;
+      addLike(id);
+    }
+  });
+}
